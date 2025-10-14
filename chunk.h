e1d40cd1c4d850ed86f262581ada274a8a650298
Ian Laidlaw <ian@kaboodles.com>
2025-10-14 12:00:32 -0400

    init commit

---
+++ chunk.h
@@ -0,0 +1,27 @@
+#ifndef CLOX_CHUNK_H
+#define CLOX_CHUNK_H
+
+#include "common.h"
+#include "value.h"
+
+#include <stdint.h>
+
+typedef enum {
+  OP_CONSTANT,
+  OP_RETURN,
+} OpCode;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t* code;
+  int* lines;
+  ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+void freeChunk(Chunk* chunk);
+
+#endif // CLOX_CHUNK_H
