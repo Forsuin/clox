e47b5a504af11f2411193dfa22a34716a8ecc516
Ian Laidlaw <ian@kaboodles.com>
2025-10-15 11:40:56 -0400

    Added scanner and compiler

--- vm.c
+++ vm.c
@@ -1,9 +1,10 @@
 #include "vm.h"
 
-#include "common.h"
-#include "debug.h"
-
 #include <stdio.h>
+
+#include "common.h"
+#include "compiler.h"
+#include "debug.h"
 
 VM vm;
 
@@ -81,11 +82,10 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk)
+InterpretResult interpret(const char* source)
 {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+  compile(source);
+  return INTERPRET_OK;
 }
 
 void push(Value value)
