9ff0b99213e85d2f39d749b1502c7a18c0dd6267
Ian Laidlaw <ian@kaboodles.com>
2025-10-17 16:24:10 -0400

    Added generic objects, currently only strings

---
+++ object.c
@@ -0,0 +1,45 @@
+#include "object.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "value.h"
+#include "vm.h"
+
+#define ALLOCATE_OBJ(type, objectType) \
+  (type*)allocateObject(sizeof(type), objectType)
+
+static Obj* allocateObject(const size_t size, const ObjType type)
+{
+  Obj* object = (Obj*)reallocate(NULL, 0, size);
+  object->type = type;
+  return object;
+}
+
+static ObjString* allocateString(char* chars, const int length)
+{
+  ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+  string->length = length;
+  string->chars = chars;
+  return string;
+}
+
+ObjString* copyString(const char* chars, const int length)
+{
+  char* heapChars = ALLOCATE(char, length + 1);
+  memcpy(heapChars, chars, length);
+  heapChars[length] = '\0';
+  return allocateString(heapChars, length);
+}
+
+void printObject(const Value value)
+{
+  switch (OBJ_TYPE(value))
+  {
+    case OBJ_STRING:
+      printf("%s", AS_CSTRING(value));
+      break;
+  }
+}
--- value.c
+++ value.c
@@ -4,6 +4,9 @@
 #include "value.h"
 
 #include <stdio.h>
+#include <string.h>
+
+#include "object.h"
 
 bool valuesEqual(const Value a, const Value b)
 {
@@ -14,6 +17,14 @@ bool valuesEqual(const Value a, const Value b)
     case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
     case VAL_NIL: return true;
     case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+    case VAL_OBJ:
+      {
+        ObjString* aString = AS_STRING(a);
+        ObjString* bString = AS_STRING(b);
+
+        return aString->length == bString->length &&
+          memcmp(aString->chars, bString->chars, aString->length) == 0;
+      }
     default: return false; // unreachable
   }
 }
@@ -55,6 +66,8 @@ void printValue(const Value value)
     case VAL_NIL: printf("nil");
       break;
     case VAL_NUMBER: printf("%g", AS_NUMBER(value));
+      break;
+    case VAL_OBJ: printObject(value);
       break;
   }
 }
